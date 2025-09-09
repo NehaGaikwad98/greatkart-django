70a39299cf58ccd6ac4a73576bdc057b0d4fa606
Neha Gaikwad <nehasgaikwad98@gmail.com>
2025-09-09 13:21:20 +0530

    Authentication of account verifications.

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -136,3 +136,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: "danger",
 }
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'NehaGaikwad@gmail.com'
+EMAIL_HOST_PASSWORD = 'neha@123'
+EMAIL_USE_TLS = True
