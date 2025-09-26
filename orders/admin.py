6d0cb5cd50613ed6a29a4d231e90a9ed46607766
Neha Gaikwad <nehasgaikwad98@gmail.com>
2025-09-26 10:11:47 +0530

    Adding payments method in e-commerce.

---
+++ orders/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Payment,OrderProduct, Order
+# Register your models here.
+
+admin.site.register(Payment)
+admin.site.register(OrderProduct)
+admin.site.register(Order)
